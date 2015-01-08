41b74677138e2d9cbc13cb5ad323cbbf886b6d74
juliocnsouza <juliocnsouzadev@gmail.com>
2015-01-08 17:08:15 -0200

    declarada uma função login que por sua vez irá trazer a view login dentro do diretório /views/user/

--- MySimpleTwitter/public_html/nwitter/routes/users.js
+++ MySimpleTwitter/public_html/nwitter/routes/users.js
@@ -1,9 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+exports.login = function(req, res) {
+	res.render('user/login')
+}
